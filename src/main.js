621095828b010acbd65b3c8336cdd0517f3e293d
lollobianco <loreb2002@gmail.com>
2022-11-29 11:33:02 +0100

    Corretta importazione bootstrap

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 //BOOTSTRAP
-import bootstrap from './bootstrap/dist/css/bootstrap.min.css'
-import bootstrap from './bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 
 //FONT-AWESOME
